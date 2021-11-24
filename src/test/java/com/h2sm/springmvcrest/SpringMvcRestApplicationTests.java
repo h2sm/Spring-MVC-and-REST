dae5cac785e17380e1cf17da88c30fc4da5d9fd2
Egor Fedorenko <fedore.egor@gmail.com>
2021-11-24 20:40:28 +0300

    init commit 2

---
+++ src/test/java/com/h2sm/springmvcrest/SpringMvcRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h2sm.springmvcrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
